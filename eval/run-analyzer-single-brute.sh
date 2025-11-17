a0d65732c49f59808a29dfe916e83c35eeb65592
jra <joao.amado@tecnico.ulisboa.pt>
2025-11-17 11:39:14 +0000

    Add eval run scripts for 1/2048 and 1/4096.

--- eval/run-analyzer-single-brute.sh
+++ eval/run-analyzer-single-brute.sh
@@ -4,89 +4,124 @@
 ./analyzer-single.py -d hypervision -c brute -t charrdos -f charrdos-0-cp-dp-sim-timeout-5-sampl-64
 ./analyzer-single.py -d hypervision -c brute -t charrdos -f charrdos-0-cp-dp-sim-timeout-5-sampl-256
 ./analyzer-single.py -d hypervision -c brute -t charrdos -f charrdos-0-cp-dp-sim-timeout-5-sampl-1024
+./analyzer-single.py -d hypervision -c brute -t charrdos -f charrdos-0-cp-dp-sim-timeout-5-sampl-2048
+./analyzer-single.py -d hypervision -c brute -t charrdos -f charrdos-0-cp-dp-sim-timeout-5-sampl-4096
 
 ./analyzer-single.py -d hypervision -c brute -t cldaprdos -f cldaprdos-0-cp-dp-sim-timeout-5-sampl-1
 ./analyzer-single.py -d hypervision -c brute -t cldaprdos -f cldaprdos-0-cp-dp-sim-timeout-5-sampl-64
 ./analyzer-single.py -d hypervision -c brute -t cldaprdos -f cldaprdos-0-cp-dp-sim-timeout-5-sampl-256
 ./analyzer-single.py -d hypervision -c brute -t cldaprdos -f cldaprdos-0-cp-dp-sim-timeout-5-sampl-1024
+./analyzer-single.py -d hypervision -c brute -t cldaprdos -f cldaprdos-0-cp-dp-sim-timeout-5-sampl-2048
+./analyzer-single.py -d hypervision -c brute -t cldaprdos -f cldaprdos-0-cp-dp-sim-timeout-5-sampl-4096
 
 ./analyzer-single.py -d hypervision -c brute -t dnsrdos -f dnsrdos-0-cp-dp-sim-timeout-5-sampl-1
 ./analyzer-single.py -d hypervision -c brute -t dnsrdos -f dnsrdos-0-cp-dp-sim-timeout-5-sampl-64
 ./analyzer-single.py -d hypervision -c brute -t dnsrdos -f dnsrdos-0-cp-dp-sim-timeout-5-sampl-256
 ./analyzer-single.py -d hypervision -c brute -t dnsrdos -f dnsrdos-0-cp-dp-sim-timeout-5-sampl-1024
+./analyzer-single.py -d hypervision -c brute -t dnsrdos -f dnsrdos-0-cp-dp-sim-timeout-5-sampl-2048
+./analyzer-single.py -d hypervision -c brute -t dnsrdos -f dnsrdos-0-cp-dp-sim-timeout-5-sampl-4096
 
 ./analyzer-single.py -d hypervision -c brute -t dnsscan -f dnsscan-0-cp-dp-sim-timeout-5-sampl-1
 ./analyzer-single.py -d hypervision -c brute -t dnsscan -f dnsscan-0-cp-dp-sim-timeout-5-sampl-64
 ./analyzer-single.py -d hypervision -c brute -t dnsscan -f dnsscan-0-cp-dp-sim-timeout-5-sampl-256
 ./analyzer-single.py -d hypervision -c brute -t dnsscan -f dnsscan-0-cp-dp-sim-timeout-5-sampl-1024
+./analyzer-single.py -d hypervision -c brute -t dnsscan -f dnsscan-0-cp-dp-sim-timeout-5-sampl-2048
+./analyzer-single.py -d hypervision -c brute -t dnsscan -f dnsscan-0-cp-dp-sim-timeout-5-sampl-4096
 
 ./analyzer-single.py -d hypervision -c brute -t httpscan -f httpscan-0-cp-dp-sim-timeout-5-sampl-1
 ./analyzer-single.py -d hypervision -c brute -t httpscan -f httpscan-0-cp-dp-sim-timeout-5-sampl-64
 ./analyzer-single.py -d hypervision -c brute -t httpscan -f httpscan-0-cp-dp-sim-timeout-5-sampl-256
 ./analyzer-single.py -d hypervision -c brute -t httpscan -f httpscan-0-cp-dp-sim-timeout-5-sampl-1024
+./analyzer-single.py -d hypervision -c brute -t httpscan -f httpscan-0-cp-dp-sim-timeout-5-sampl-2048
+./analyzer-single.py -d hypervision -c brute -t httpscan -f httpscan-0-cp-dp-sim-timeout-5-sampl-4096
 
 ./analyzer-single.py -d hypervision -c brute -t httpsscan -f httpsscan-0-cp-dp-sim-timeout-5-sampl-1
 ./analyzer-single.py -d hypervision -c brute -t httpsscan -f httpsscan-0-cp-dp-sim-timeout-5-sampl-64
 ./analyzer-single.py -d hypervision -c brute -t httpsscan -f httpsscan-0-cp-dp-sim-timeout-5-sampl-256
 ./analyzer-single.py -d hypervision -c brute -t httpsscan -f httpsscan-0-cp-dp-sim-timeout-5-sampl-1024
+./analyzer-single.py -d hypervision -c brute -t httpsscan -f httpsscan-0-cp-dp-sim-timeout-5-sampl-2048
+./analyzer-single.py -d hypervision -c brute -t httpsscan -f httpsscan-0-cp-dp-sim-timeout-5-sampl-4096
 
 ./analyzer-single.py -d hypervision -c brute -t icmpscan -f icmpscan-0-cp-dp-sim-timeout-5-sampl-1
 ./analyzer-single.py -d hypervision -c brute -t icmpscan -f icmpscan-0-cp-dp-sim-timeout-5-sampl-64
 ./analyzer-single.py -d hypervision -c brute -t icmpscan -f icmpscan-0-cp-dp-sim-timeout-5-sampl-256
 ./analyzer-single.py -d hypervision -c brute -t icmpscan -f icmpscan-0-cp-dp-sim-timeout-5-sampl-1024
+./analyzer-single.py -d hypervision -c brute -t icmpscan -f icmpscan-0-cp-dp-sim-timeout-5-sampl-2048
+./analyzer-single.py -d hypervision -c brute -t icmpscan -f icmpscan-0-cp-dp-sim-timeout-5-sampl-4096
 
 ./analyzer-single.py -d hypervision -c brute -t icmpsdos -f icmpsdos-0-cp-dp-sim-timeout-5-sampl-1
 ./analyzer-single.py -d hypervision -c brute -t icmpsdos -f icmpsdos-0-cp-dp-sim-timeout-5-sampl-64
 ./analyzer-single.py -d hypervision -c brute -t icmpsdos -f icmpsdos-0-cp-dp-sim-timeout-5-sampl-256
 ./analyzer-single.py -d hypervision -c brute -t icmpsdos -f icmpsdos-0-cp-dp-sim-timeout-5-sampl-1024
+./analyzer-single.py -d hypervision -c brute -t icmpsdos -f icmpsdos-0-cp-dp-sim-timeout-5-sampl-2048
+./analyzer-single.py -d hypervision -c brute -t icmpsdos -f icmpsdos-0-cp-dp-sim-timeout-5-sampl-4096
 
 ./analyzer-single.py -d hypervision -c brute -t memcachedrdos -f memcachedrdos-0-cp-dp-sim-timeout-5-sampl-1
 ./analyzer-single.py -d hypervision -c brute -t memcachedrdos -f memcachedrdos-0-cp-dp-sim-timeout-5-sampl-64
 ./analyzer-single.py -d hypervision -c brute -t memcachedrdos -f memcachedrdos-0-cp-dp-sim-timeout-5-sampl-256
 ./analyzer-single.py -d hypervision -c brute -t memcachedrdos -f memcachedrdos-0-cp-dp-sim-timeout-5-sampl-1024
+./analyzer-single.py -d hypervision -c brute -t memcachedrdos -f memcachedrdos-0-cp-dp-sim-timeout-5-sampl-2048
+./analyzer-single.py -d hypervision -c brute -t memcachedrdos -f memcachedrdos-0-cp-dp-sim-timeout-5-sampl-4096
 
 ./analyzer-single.py -d hypervision -c brute -t ntprdos -f ntprdos-0-cp-dp-sim-timeout-5-sampl-1
 ./analyzer-single.py -d hypervision -c brute -t ntprdos -f ntprdos-0-cp-dp-sim-timeout-5-sampl-64
 ./analyzer-single.py -d hypervision -c brute -t ntprdos -f ntprdos-0-cp-dp-sim-timeout-5-sampl-256
 ./analyzer-single.py -d hypervision -c brute -t ntprdos -f ntprdos-0-cp-dp-sim-timeout-5-sampl-1024
+./analyzer-single.py -d hypervision -c brute -t ntprdos -f ntprdos-0-cp-dp-sim-timeout-5-sampl-2048
+./analyzer-single.py -d hypervision -c brute -t ntprdos -f ntprdos-0-cp-dp-sim-timeout-5-sampl-4096
 
 ./analyzer-single.py -d hypervision -c brute -t ntpscan -f ntpscan-0-cp-dp-sim-timeout-5-sampl-1
 ./analyzer-single.py -d hypervision -c brute -t ntpscan -f ntpscan-0-cp-dp-sim-timeout-5-sampl-64
 ./analyzer-single.py -d hypervision -c brute -t ntpscan -f ntpscan-0-cp-dp-sim-timeout-5-sampl-256
 ./analyzer-single.py -d hypervision -c brute -t ntpscan -f ntpscan-0-cp-dp-sim-timeout-5-sampl-1024
+./analyzer-single.py -d hypervision -c brute -t ntpscan -f ntpscan-0-cp-dp-sim-timeout-5-sampl-2048
+./analyzer-single.py -d hypervision -c brute -t ntpscan -f ntpscan-0-cp-dp-sim-timeout-5-sampl-4096
 
 ./analyzer-single.py -d hypervision -c brute -t riprdos -f riprdos-0-cp-dp-sim-timeout-5-sampl-1
 ./analyzer-single.py -d hypervision -c brute -t riprdos -f riprdos-0-cp-dp-sim-timeout-5-sampl-64
 ./analyzer-single.py -d hypervision -c brute -t riprdos -f riprdos-0-cp-dp-sim-timeout-5-sampl-256
 ./analyzer-single.py -d hypervision -c brute -t riprdos -f riprdos-0-cp-dp-sim-timeout-5-sampl-1024
+./analyzer-single.py -d hypervision -c brute -t riprdos -f riprdos-0-cp-dp-sim-timeout-5-sampl-2048
+./analyzer-single.py -d hypervision -c brute -t riprdos -f riprdos-0-cp-dp-sim-timeout-5-sampl-4096
 
 ./analyzer-single.py -d hypervision -c brute -t rstsdos -f rstsdos-0-cp-dp-sim-timeout-5-sampl-1
 ./analyzer-single.py -d hypervision -c brute -t rstsdos -f rstsdos-0-cp-dp-sim-timeout-5-sampl-64
 ./analyzer-single.py -d hypervision -c brute -t rstsdos -f rstsdos-0-cp-dp-sim-timeout-5-sampl-256
 ./analyzer-single.py -d hypervision -c brute -t rstsdos -f rstsdos-0-cp-dp-sim-timeout-5-sampl-1024
+./analyzer-single.py -d hypervision -c brute -t rstsdos -f rstsdos-0-cp-dp-sim-timeout-5-sampl-2048
+./analyzer-single.py -d hypervision -c brute -t rstsdos -f rstsdos-0-cp-dp-sim-timeout-5-sampl-4096
 
 ./analyzer-single.py -d hypervision -c brute -t sqlscan -f sqlscan-0-cp-dp-sim-timeout-5-sampl-1
 ./analyzer-single.py -d hypervision -c brute -t sqlscan -f sqlscan-0-cp-dp-sim-timeout-5-sampl-64
 ./analyzer-single.py -d hypervision -c brute -t sqlscan -f sqlscan-0-cp-dp-sim-timeout-5-sampl-256
 ./analyzer-single.py -d hypervision -c brute -t sqlscan -f sqlscan-0-cp-dp-sim-timeout-5-sampl-1024
+./analyzer-single.py -d hypervision -c brute -t sqlscan -f sqlscan-0-cp-dp-sim-timeout-5-sampl-2048
+./analyzer-single.py -d hypervision -c brute -t sqlscan -f sqlscan-0-cp-dp-sim-timeout-5-sampl-4096
 
 ./analyzer-single.py -d hypervision -c brute -t ssdprdos -f ssdprdos-0-cp-dp-sim-timeout-5-sampl-1
 ./analyzer-single.py -d hypervision -c brute -t ssdprdos -f ssdprdos-0-cp-dp-sim-timeout-5-sampl-64
 ./analyzer-single.py -d hypervision -c brute -t ssdprdos -f ssdprdos-0-cp-dp-sim-timeout-5-sampl-256
 ./analyzer-single.py -d hypervision -c brute -t ssdprdos -f ssdprdos-0-cp-dp-sim-timeout-5-sampl-1024
+./analyzer-single.py -d hypervision -c brute -t ssdprdos -f ssdprdos-0-cp-dp-sim-timeout-5-sampl-2048
+./analyzer-single.py -d hypervision -c brute -t ssdprdos -f ssdprdos-0-cp-dp-sim-timeout-5-sampl-4096
 
 ./analyzer-single.py -d hypervision -c brute -t sshscan -f sshscan-0-cp-dp-sim-timeout-5-sampl-1
 ./analyzer-single.py -d hypervision -c brute -t sshscan -f sshscan-0-cp-dp-sim-timeout-5-sampl-64
 ./analyzer-single.py -d hypervision -c brute -t sshscan -f sshscan-0-cp-dp-sim-timeout-5-sampl-256
 ./analyzer-single.py -d hypervision -c brute -t sshscan -f sshscan-0-cp-dp-sim-timeout-5-sampl-1024
+./analyzer-single.py -d hypervision -c brute -t sshscan -f sshscan-0-cp-dp-sim-timeout-5-sampl-2048
+./analyzer-single.py -d hypervision -c brute -t sshscan -f sshscan-0-cp-dp-sim-timeout-5-sampl-4096
 
 ./analyzer-single.py -d hypervision -c brute -t synsdos -f synsdos-0-cp-dp-sim-timeout-5-sampl-1
 ./analyzer-single.py -d hypervision -c brute -t synsdos -f synsdos-0-cp-dp-sim-timeout-5-sampl-64
 ./analyzer-single.py -d hypervision -c brute -t synsdos -f synsdos-0-cp-dp-sim-timeout-5-sampl-256
 ./analyzer-single.py -d hypervision -c brute -t synsdos -f synsdos-0-cp-dp-sim-timeout-5-sampl-1024
+./analyzer-single.py -d hypervision -c brute -t synsdos -f synsdos-0-cp-dp-sim-timeout-5-sampl-2048
+./analyzer-single.py -d hypervision -c brute -t synsdos -f synsdos-0-cp-dp-sim-timeout-5-sampl-4096
 
 ./analyzer-single.py -d hypervision -c brute -t udpsdos -f udpsdos-0-cp-dp-sim-timeout-5-sampl-1
 ./analyzer-single.py -d hypervision -c brute -t udpsdos -f udpsdos-0-cp-dp-sim-timeout-5-sampl-64
 ./analyzer-single.py -d hypervision -c brute -t udpsdos -f udpsdos-0-cp-dp-sim-timeout-5-sampl-256
 ./analyzer-single.py -d hypervision -c brute -t udpsdos -f udpsdos-0-cp-dp-sim-timeout-5-sampl-1024
-
+./analyzer-single.py -d hypervision -c brute -t udpsdos -f udpsdos-0-cp-dp-sim-timeout-5-sampl-2048
+./analyzer-single.py -d hypervision -c brute -t udpsdos -f udpsdos-0-cp-dp-sim-timeout-5-sampl-4096
--- eval/run-analyzer-single-web.sh
+++ eval/run-analyzer-single-web.sh
@@ -4,44 +4,61 @@
 ./analyzer-single.py -d hypervision -c web -t agentinject -f agentinject-0-cp-dp-sim-timeout-5-sampl-64
 ./analyzer-single.py -d hypervision -c web -t agentinject -f agentinject-0-cp-dp-sim-timeout-5-sampl-256
 ./analyzer-single.py -d hypervision -c web -t agentinject -f agentinject-0-cp-dp-sim-timeout-5-sampl-1024
+./analyzer-single.py -d hypervision -c web -t agentinject -f agentinject-0-cp-dp-sim-timeout-5-sampl-2048
+./analyzer-single.py -d hypervision -c web -t agentinject -f agentinject-0-cp-dp-sim-timeout-5-sampl-4096
 
 ./analyzer-single.py -d hypervision -c web -t codeinject -f codeinject-0-cp-dp-sim-timeout-5-sampl-1
 ./analyzer-single.py -d hypervision -c web -t codeinject -f codeinject-0-cp-dp-sim-timeout-5-sampl-64
 ./analyzer-single.py -d hypervision -c web -t codeinject -f codeinject-0-cp-dp-sim-timeout-5-sampl-256
 ./analyzer-single.py -d hypervision -c web -t codeinject -f codeinject-0-cp-dp-sim-timeout-5-sampl-1024
+./analyzer-single.py -d hypervision -c web -t codeinject -f codeinject-0-cp-dp-sim-timeout-5-sampl-2048
+./analyzer-single.py -d hypervision -c web -t codeinject -f codeinject-0-cp-dp-sim-timeout-5-sampl-4096
 
 ./analyzer-single.py -d hypervision -c web -t csfr -f csfr-0-cp-dp-sim-timeout-5-sampl-1
 ./analyzer-single.py -d hypervision -c web -t csfr -f csfr-0-cp-dp-sim-timeout-5-sampl-64
 ./analyzer-single.py -d hypervision -c web -t csfr -f csfr-0-cp-dp-sim-timeout-5-sampl-256
 ./analyzer-single.py -d hypervision -c web -t csfr -f csfr-0-cp-dp-sim-timeout-5-sampl-1024
+./analyzer-single.py -d hypervision -c web -t csfr -f csfr-0-cp-dp-sim-timeout-5-sampl-2048
+./analyzer-single.py -d hypervision -c web -t csfr -f csfr-0-cp-dp-sim-timeout-5-sampl-4096
 
 ./analyzer-single.py -d hypervision -c web -t paraminject -f paraminject-0-cp-dp-sim-timeout-5-sampl-1
 ./analyzer-single.py -d hypervision -c web -t paraminject -f paraminject-0-cp-dp-sim-timeout-5-sampl-64
 ./analyzer-single.py -d hypervision -c web -t paraminject -f paraminject-0-cp-dp-sim-timeout-5-sampl-256
 ./analyzer-single.py -d hypervision -c web -t paraminject -f paraminject-0-cp-dp-sim-timeout-5-sampl-1024
+./analyzer-single.py -d hypervision -c web -t paraminject -f paraminject-0-cp-dp-sim-timeout-5-sampl-2048
+./analyzer-single.py -d hypervision -c web -t paraminject -f paraminject-0-cp-dp-sim-timeout-5-sampl-4096
 
 ./analyzer-single.py -d hypervision -c web -t spam1 -f spam1-0-cp-dp-sim-timeout-5-sampl-1
 ./analyzer-single.py -d hypervision -c web -t spam1 -f spam1-0-cp-dp-sim-timeout-5-sampl-64
 ./analyzer-single.py -d hypervision -c web -t spam1 -f spam1-0-cp-dp-sim-timeout-5-sampl-256
 ./analyzer-single.py -d hypervision -c web -t spam1 -f spam1-0-cp-dp-sim-timeout-5-sampl-1024
+./analyzer-single.py -d hypervision -c web -t spam1 -f spam1-0-cp-dp-sim-timeout-5-sampl-2048
+./analyzer-single.py -d hypervision -c web -t spam1 -f spam1-0-cp-dp-sim-timeout-5-sampl-4096
 
 ./analyzer-single.py -d hypervision -c web -t spam100 -f spam100-0-cp-dp-sim-timeout-5-sampl-1
 ./analyzer-single.py -d hypervision -c web -t spam100 -f spam100-0-cp-dp-sim-timeout-5-sampl-64
 ./analyzer-single.py -d hypervision -c web -t spam100 -f spam100-0-cp-dp-sim-timeout-5-sampl-256
 ./analyzer-single.py -d hypervision -c web -t spam100 -f spam100-0-cp-dp-sim-timeout-5-sampl-1024
+./analyzer-single.py -d hypervision -c web -t spam100 -f spam100-0-cp-dp-sim-timeout-5-sampl-2048
+./analyzer-single.py -d hypervision -c web -t spam100 -f spam100-0-cp-dp-sim-timeout-5-sampl-4096
 
 ./analyzer-single.py -d hypervision -c web -t sslscan -f sslscan-0-cp-dp-sim-timeout-5-sampl-1
 ./analyzer-single.py -d hypervision -c web -t sslscan -f sslscan-0-cp-dp-sim-timeout-5-sampl-64
 ./analyzer-single.py -d hypervision -c web -t sslscan -f sslscan-0-cp-dp-sim-timeout-5-sampl-256
 ./analyzer-single.py -d hypervision -c web -t sslscan -f sslscan-0-cp-dp-sim-timeout-5-sampl-1024
+./analyzer-single.py -d hypervision -c web -t sslscan -f sslscan-0-cp-dp-sim-timeout-5-sampl-2048
+./analyzer-single.py -d hypervision -c web -t sslscan -f sslscan-0-cp-dp-sim-timeout-5-sampl-4096
 
 ./analyzer-single.py -d hypervision -c web -t webshell -f webshell-0-cp-dp-sim-timeout-5-sampl-1
 ./analyzer-single.py -d hypervision -c web -t webshell -f webshell-0-cp-dp-sim-timeout-5-sampl-64
 ./analyzer-single.py -d hypervision -c web -t webshell -f webshell-0-cp-dp-sim-timeout-5-sampl-256
 ./analyzer-single.py -d hypervision -c web -t webshell -f webshell-0-cp-dp-sim-timeout-5-sampl-1024
+./analyzer-single.py -d hypervision -c web -t webshell -f webshell-0-cp-dp-sim-timeout-5-sampl-2048
+./analyzer-single.py -d hypervision -c web -t webshell -f webshell-0-cp-dp-sim-timeout-5-sampl-4096
 
 ./analyzer-single.py -d hypervision -c web -t xss -f xss-0-cp-dp-sim-timeout-5-sampl-1
 ./analyzer-single.py -d hypervision -c web -t xss -f xss-0-cp-dp-sim-timeout-5-sampl-64
 ./analyzer-single.py -d hypervision -c web -t xss -f xss-0-cp-dp-sim-timeout-5-sampl-256
 ./analyzer-single.py -d hypervision -c web -t xss -f xss-0-cp-dp-sim-timeout-5-sampl-1024
-
+./analyzer-single.py -d hypervision -c web -t xss -f xss-0-cp-dp-sim-timeout-5-sampl-2048
+./analyzer-single.py -d hypervision -c web -t xss -f xss-0-cp-dp-sim-timeout-5-sampl-4096
