acb2d6393cd97ab049153299c2c88d3453982e24
jra <joao.amado@tecnico.ulisboa.pt>
2026-02-10 11:39:24 +0000

    Run scripts cleanup.

--- run.sh
+++ run.sh
@@ -7,7 +7,7 @@ DATETIME="$(date +%Y-%m-%d-%H-%M-%S-%3N)"
 DATASET=$1
 TRACE=$2
 
-# sudo -E ./build/debug/hypervision conf/$DATASET/$TRACE.json cp | tee "eval/logs/$DATASET/$TRACE/$TRACE-$DATETIME.log"
+sudo -E ./build/debug/hypervision conf/$DATASET/$TRACE.json cp | tee "eval/logs/$DATASET/$TRACE/$TRACE-$DATETIME.log"
 # sudo -E ./build/debug/hypervision conf/$DATASET/$TRACE.json cp > flows_all.txt
-sudo -E gdb -ex="set confirm off" -ex=r --args ./build/debug/hypervision conf/$DATASET/$TRACE.json cp | tee "eval/logs/$DATASET/$TRACE/$TRACE-$DATETIME.log"
+# sudo -E gdb -ex="set confirm off" -ex=r --args ./build/debug/hypervision conf/$DATASET/$TRACE.json cp | tee "eval/logs/$DATASET/$TRACE/$TRACE-$DATETIME.log"
 # sudo -E gdb -ex="set confirm off" -ex="break src/sample.h:448" -ex=r --args ./build/debug/hypervision conf/$DATASET/$TRACE.json cp | tee "eval/logs/$DATASET/$TRACE/$TRACE-$DATETIME.log"
